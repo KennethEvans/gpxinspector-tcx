02132f18eeedbac37d8100df6d72d891ab19ab24
Kenneth Evans <ken@kenevans.net>
2019-01-24 01:37:21 -0500

    Changed TcxConverter to have the new getPreferredExtension() method. Implemented adding the TrackPointExtension if hr, cad, or dist are in the TCX file.  It is currently failing to marshal this extension, however when saving.

--- src/net/kenevans/gpxinspector/tcx/converters/TcxConverter.java
+++ src/net/kenevans/gpxinspector/tcx/converters/TcxConverter.java
@@ -9,24 +9,21 @@ import javax.xml.datatype.XMLGregorianCalendar;
 
 import net.kenevans.gpxinspector.converters.IGpxConverter;
 import net.kenevans.gpxinspector.utils.SWTUtils;
+import net.kenevans.gpxtrackpointextensionsv1.ExtensionsType;
 import net.kenevans.gpxtrackpointextensionsv1.GpxType;
+import net.kenevans.gpxtrackpointextensionsv1.TrackPointExtensionT;
 import net.kenevans.gpxtrackpointextensionsv1.TrkType;
 import net.kenevans.gpxtrackpointextensionsv1.TrksegType;
 import net.kenevans.gpxtrackpointextensionsv1.WptType;
 import net.kenevans.gpxtrackpointextensionsv1.parser.GPXParser;
-import net.kenevans.trainingcenterdatabasev2.AbstractSourceT;
 import net.kenevans.trainingcenterdatabasev2.ActivityLapT;
 import net.kenevans.trainingcenterdatabasev2.ActivityListT;
 import net.kenevans.trainingcenterdatabasev2.ActivityT;
 import net.kenevans.trainingcenterdatabasev2.HeartRateInBeatsPerMinuteT;
-import net.kenevans.trainingcenterdatabasev2.PlanT;
 import net.kenevans.trainingcenterdatabasev2.PositionT;
-import net.kenevans.trainingcenterdatabasev2.SensorStateT;
 import net.kenevans.trainingcenterdatabasev2.TrackT;
 import net.kenevans.trainingcenterdatabasev2.TrackpointT;
 import net.kenevans.trainingcenterdatabasev2.TrainingCenterDatabaseT;
-import net.kenevans.trainingcenterdatabasev2.TrainingT;
-import net.kenevans.trainingcenterdatabasev2.TrainingTypeT;
 import net.kenevans.trainingcenterdatabasev2.parser.TCXParser;
 
 /*
@@ -52,6 +49,17 @@ public class TcxConverter implements IGpxConverter {
 			retVal += "*" + ext;
 		}
 		return retVal;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * net.kenevans.gpxinspector.converters.IGpxConverter#getPreferredExtension()
+	 */
+	@Override
+	public String getPreferredExtension() {
+		return extensions[0].replaceFirst("\\.", "");
 	}
 
 	/*
@@ -121,15 +129,19 @@ public class TcxConverter implements IGpxConverter {
 		Short cad, hr;
 		Double ele, dist;
 		double lat, lon;
+		XMLGregorianCalendar time;
 		HeartRateInBeatsPerMinuteT hrBpm;
 		PositionT position;
-		SensorStateT sensorState;
-		XMLGregorianCalendar time, id;
-		AbstractSourceT creator;
-		String creatorName, trainingPlanName, trainingPlanType;
-		TrainingT training;
-		PlanT plan;
-		TrainingTypeT trainingType;
+		TrackPointExtensionT tpExt;
+		ExtensionsType extType;
+
+//		XMLGregorianCalendar id;
+//		SensorStateT sensorState;
+//		AbstractSourceT creator;
+//		String creatorName, trainingPlanName, trainingPlanType;
+//		TrainingT training;
+//		PlanT plan;
+//		TrainingTypeT trainingType;
 
 		boolean error = false;
 		TrkType trkType = null;
@@ -139,57 +151,35 @@ public class TcxConverter implements IGpxConverter {
 		activityList = tcx.getActivities();
 		activities = activityList.getActivity();
 		for (ActivityT activity : activities) {
-			id = activity.getId();
-			creator = activity.getCreator();
-			creatorName = creator.getName();
-			
-			training = activity.getTraining();
-			trainingType = null;
-			trainingPlanName=null;
-			trainingPlanType = null;
-			if (training != null) {
-				plan = training.getPlan();
-				if (plan != null) {
-					trainingType = plan.getType();
-					trainingPlanName = plan.getName();
-					if(trainingType != null) {
-						trainingPlanType = trainingType.value();
-					}
-				}
-			}
-			System.out.println("Activity : " + id + " creator=" + creatorName);
-			System.out.println("  Plan: " + trainingPlanName + " Type: " + trainingPlanType);
+//			id = activity.getId();
+//			creator = activity.getCreator();
+//			creatorName = creator.getName();
+
+//			training = activity.getTraining();
+//			trainingType = null;
+//			trainingPlanName = null;
+//			trainingPlanType = null;
+//			if (training != null) {
+//				plan = training.getPlan();
+//				if (plan != null) {
+//					trainingType = plan.getType();
+//					trainingPlanName = plan.getName();
+//					if (trainingType != null) {
+//						trainingPlanType = trainingType.value();
+//					}
+//				}
+//			}
 			laps = activity.getLap();
 			for (ActivityLapT lap : laps) {
 				tracks = lap.getTrack();
-				int nTracks = 0;
 				for (TrackT track : tracks) {
-					nTracks++;
 					trkType = new TrkType();
-					if (trkType == null) {
-						SWTUtils.errMsg("Track " + nTracks + ": Cannot create track:\n" + file.getName());
-						error = true;
-						break;
-					}
 					gpx.getTrk().add(trkType);
 					trksegType = new TrksegType();
-					if (trksegType == null) {
-						SWTUtils.errMsg("Track " + nTracks + ": Cannot create track segment:\n" + file.getName());
-						error = true;
-						break;
-					}
 					trkType.getTrkseg().add(trksegType);
 					trackPoints = track.getTrackpoint();
-					int nTrackPoints = 0;
 					for (TrackpointT trackPoint : trackPoints) {
-						nTrackPoints++;
 						wptType = new WptType();
-						if (wptType == null) {
-							SWTUtils.errMsg("Track " + nTracks + "TrackPoint " + nTrackPoints + ": Cannot trackpoint:\n"
-									+ file.getName());
-							error = true;
-							break;
-						}
 						position = trackPoint.getPosition();
 						if (position != null) {
 							lat = position.getLatitudeDegrees();
@@ -201,28 +191,47 @@ public class TcxConverter implements IGpxConverter {
 						} else {
 							continue;
 						}
+
 						ele = trackPoint.getAltitudeMeters();
+						if (ele != null) {
+							wptType.setEle(new BigDecimal(ele));
+						}
+
+						time = trackPoint.getTime();
+						if (time != null) {
+							wptType.setTime(time);
+						}
+
 						cad = trackPoint.getCadence();
 						dist = trackPoint.getDistanceMeters();
 						hrBpm = trackPoint.getHeartRateBpm();
 						if (hrBpm != null) {
 							hr = hrBpm.getValue();
 						} else {
-							hr = null;
+							hr = 0;
 						}
-						if (ele != null) {
-							wptType.setEle(new BigDecimal(ele));
-						}
-						time = trackPoint.getTime();
-						if (time != null) {
-							wptType.setTime(time);
+						if ((hrBpm != null || cad != null || dist != null)) {
+							tpExt = new TrackPointExtensionT();
+							if (hrBpm != null) {
+								tpExt.setHr(hr);
+							}
+							if (cad != null) {
+								tpExt.setCad(cad);
+							}
+							if (dist != null) {
+								tpExt.setDepth(dist);
+							}
+							extType = new ExtensionsType();
+							extType.getAny().add(tpExt);
+							wptType.setExtensions(extType);
 						}
 					}
 				}
 			}
 		}
-		if (error)
+		if (error) {
 			return null;
+		}
 		return gpx;
 	}
 
